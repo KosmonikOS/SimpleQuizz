cbdcdfd0d2b3b4f97af7480c1c90d7522976307b
Nikita <nik.lizard.mobile@gmail.com>
2022-09-14 19:13:22 +0400

    Add participant client

---
+++ SimpleQuizz_Prototype_Participant/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+Console.WriteLine("Введите код викторины , чтобы присоединиться");
+var quizzId = Console.ReadLine();
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://localhost:7195/quizzes")
+    .WithAutomaticReconnect().Build();
+var hostId = "";
+
+await connection.StartAsync();
+
+await connection.InvokeAsync("ConnectToQuizz", quizzId);
+
+connection.On("GetHostId",(string newHostId) =>
+{
+    hostId = newHostId;
+});
+
+connection.On("NextQuestion", async () =>
+{
+    if (!string.IsNullOrEmpty(hostId))
+    {
+        Console.Clear();
+        Console.WriteLine("Выбирите вариант ответа (1-4)");
+        var answer = Console.ReadLine();
+        await connection.InvokeAsync("SendAnswer", hostId,answer);
+        Console.WriteLine("Ждем нового вопроса");
+    }
+});
+
+Console.WriteLine("Ждем начало викторины");
+
+while (true) ;
