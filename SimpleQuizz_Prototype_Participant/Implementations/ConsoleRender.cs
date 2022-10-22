bc09372603d55c8255b3b8e0bc95ca849360d7bd
Nikita <nik.lizard.mobile@gmail.com>
2022-10-22 22:00:03 +0400

    Create IRender for participant with implementation for console

---
+++ SimpleQuizz_Prototype_Participant/Implementations/ConsoleRender.cs
@@ -0,0 +1,54 @@
+﻿using SimpleQuizz_Prototype_Host.Helpers;
+using SimpleQuizz_Prototype_Participant.Interfaces;
+
+namespace SimpleQuizz_Prototype_Participant.Implementations;
+internal class ConsoleRender : IRender
+{
+    public void PrintStartInfo()
+    {
+        Console.Clear();
+        Console.WriteLine("Добро пожаловать в прототип SimpleQuizz");
+        Console.WriteLine("Нажмите Enter, чтобы продолжить");
+        Console.ReadLine();
+    }
+    public string RenderQuizzIdInsertion()
+    {
+        Console.Clear();
+        Console.WriteLine("Введите код викторины, к которой хотите подключиться");
+        var quizzId = Console.ReadLine();
+        ConsoleInputHelper.CheckInsertFailure(ref quizzId, x =>
+                x.Length != 7 || string.IsNullOrEmpty(x));
+        return quizzId;
+    }
+    public string RenderParticipantNameInsertion()
+    {
+        Console.Clear();
+        Console.WriteLine("Придумайте имя участника");
+        var name = Console.ReadLine();
+        ConsoleInputHelper.CheckInsertFailure(ref name, string.IsNullOrEmpty);
+        return name;
+    }
+    public int RenderAnswerInsertion()
+    {
+        Console.Clear();
+        Console.WriteLine("Введите свой вариант ответа");
+        var answer = Console.ReadLine();
+        return ConsoleInputHelper.CheckConvertionFailure<int>(answer,Int32.TryParse);
+    }
+    public void PrintQuestionOptions()
+    {
+        Console.Clear();
+        Console.WriteLine("Выбирите вариант ответа из списка");
+    }
+    public void PrintEndInfo()
+    {
+        Console.Clear();
+        Console.WriteLine("Спасибо за участие в викторине");
+    }
+
+    public void PrintWaitingInfo()
+    {
+        Console.Clear();
+        Console.WriteLine("Ожидаем начало викторины");
+    }
+}
---
+++ SimpleQuizz_Prototype_Participant/Interfaces/IRender.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleQuizz_Prototype_Participant.Interfaces;
+internal interface IRender
+{
+    public void PrintStartInfo();
+    public string RenderQuizzIdInsertion();
+    public string RenderParticipantNameInsertion();
+    public int RenderAnswerInsertion();
+    public void PrintQuestionOptions();
+    public void PrintEndInfo();
+    public void PrintWaitingInfo();
+}
+
