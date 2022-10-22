296efc8ed4130022e96d2648125dc4f4ddd30b1e
Nikita <nik.lizard.mobile@gmail.com>
2022-10-22 21:59:11 +0400

    Create IParticipantHubConnection with implementation for Asp.NetCore

---
+++ SimpleQuizz_Prototype_Participant/Implementations/AspNetCoreParticipantHubConnection.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using SimpleQuizz_Prototype_Participant.Interfaces;
+
+namespace SimpleQuizz_Prototype_Participant.Implementations;
+internal class AspNetCoreParticipantHubConnection : IParticipantHubConnection
+{
+    private string connectionString = "https://localhost:7195/quizzes";
+    private HubConnection connection;
+    private string? hostId;
+    public AspNetCoreParticipantHubConnection()
+    {
+        connection = new HubConnectionBuilder()
+            .WithUrl(connectionString)
+            .WithAutomaticReconnect().Build();
+        SubscribeOnEvent<string>("GetHostId", HandleGetHostId);
+    }
+    public string HostId { set { hostId = value; } }
+    public async Task ConnectToHubAsync()
+    {
+        await connection.StartAsync();
+    }
+    public async Task DisconnectFromHubAsync()
+    {
+        await connection.StopAsync();
+    }
+    public async Task ConnectToQuizzAsync(string quizzId)
+    {
+        await connection.SendAsync("ConnectToQuizz", quizzId);
+    }
+    public async Task SendParticipantNameAsync(string name)
+    {
+        await connection.SendAsync("SendParticipantName", hostId, name);
+    }
+    public async Task SendAnswerAsync(int answer)
+    {
+        await connection.SendAsync("SendAnswerToHost", hostId, answer.ToString());
+    }
+    public void SubscribeOnEvent<T>(string eventName, Func<T, Task> handler)
+    {
+        connection.On(eventName, handler);
+    }
+    public void SubscribeOnEvent(string eventName, Func<Task> handler)
+    {
+        connection.On(eventName, handler);
+    }
+    public void SubscribeOnEvent<T1, T2>(string eventName, Func<T1, T2, Task> handler)
+    {
+        connection.On(eventName, handler);
+    }
+    public void SubscribeOnEvent<T>(string eventName, Action<T> handler)
+    {
+        connection.On(eventName, handler);
+    }
+    public void SubscribeOnEvent<T1, T2>(string eventName, Action<T1, T2> handler)
+    {
+        connection.On(eventName, handler);
+    }
+    private void HandleGetHostId(string hostId)
+    {
+        this.hostId = hostId;
+    }
+}
---
+++ SimpleQuizz_Prototype_Participant/Interfaces/IParticipantHubConnection.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleQuizz_Prototype_Participant.Interfaces;
+internal interface IParticipantHubConnection
+{
+    public Task ConnectToHubAsync();
+    public Task DisconnectFromHubAsync();
+    public Task ConnectToQuizzAsync(string quizzId);
+    public Task SendParticipantNameAsync(string name);
+    public Task SendAnswerAsync(int answer);
+    public void SubscribeOnEvent<T>(string eventName, Func<T, Task> handler);
+    public void SubscribeOnEvent(string eventName, Func<Task> handler);
+    public void SubscribeOnEvent<T1, T2>(string eventName, Func<T1, T2, Task> handler);
+    public void SubscribeOnEvent<T>(string eventName, Action<T> handler);
+    public void SubscribeOnEvent<T1, T2>(string eventName, Action<T1, T2> handler);
+}
